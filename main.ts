224dd2c35e83515d9b17b6ce020c65c2be98b23b
fcoisaias <65827476+fcoisaias@users.noreply.github.com>
2021-10-11 09:33:42 -0700

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,18 +4,13 @@ input.onButtonPressed(Button.A, function () {
     basic.showNumber(contar)
     for (let index = 0; index <= contar; index++) {
         basic.showNumber(index)
-        basic.pause(500)
-        basic.clearScreen()
         for (let index2 = 0; index2 < index; index2++) {
             music.playTone(262, music.beat(BeatFraction.Half))
+            basic.pause(500)
         }
+        basic.clearScreen()
     }
     basic.showString("A buscar!!!")
     music.playMelody("C5 B A A B G A F ", 120)
-})
-input.onButtonPressed(Button.B, function () {
-	
-})
-basic.forever(function () {
-	
+    basic.clearScreen()
 })
